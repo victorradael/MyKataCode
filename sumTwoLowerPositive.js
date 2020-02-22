744b88676660e0ff3d7b749d1c5b36b2295ecd5b
Victor Radael <victorradael@hotmail.com>
2020-02-22 15:58:07 -0300

    Code that takes the two lowest values ​​of an array and adds

---
+++ sumTwoLowerPositive.js
@@ -0,0 +1,19 @@
+// Create a function that returns the sum of the two lowest 
+// positive numbers given an array of minimum 4 positive integers. 
+// No floats or non-positive integers will be passed.
+// For example, when an array is passed like [19, 5, 42, 2, 77], 
+// the output should be 7.
+
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+function sumTwoSmallestNumbers(numbers) {  
+    numbers.sort((a,b) => a - b);
+    console.log(numbers)
+    return numbers[0] + numbers[1];
+};
+
+sumTwoSmallestNumbers([5, 8, 12, 19, 22])  // 13 , "Sum should be 13");
+sumTwoSmallestNumbers([15, 28, 4, 2, 43])  // 6 , "Sum should be 6");
+sumTwoSmallestNumbers([3, 87, 45, 12, 7])  // 10 , "Sum should be 10");
+sumTwoSmallestNumbers([23, 71, 33, 82, 1]) // 24 , "Sum should be 24");
+sumTwoSmallestNumbers([52, 76, 14, 12, 4]) // 16 , "Sum should be 16");
